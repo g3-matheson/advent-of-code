a030eeeaf1dcd706a9421dfd25fef2a00709ca0b
g3-matheson <g3.matheson@gmail.com>
2025-12-08 04:31:44 -0500

    remove AoC comments

--- 2025/2.cpp
+++ 2025/2.cpp
@@ -9,23 +9,6 @@ using namespace std;
 	Day 2: "Gift Shop"
 	Input file contains product ID ranges left to check
 	Invalid IDs are made of a number repeated twice, or that start with 0
-
-	e.g for ranges:
-	11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
-	1698522-1698528,446443-446449,38593856-38593862,565653-565659,
-	824824821-824824827,2121212118-2121212124
-
-    11-22 has two invalid IDs, 11 and 22.
-    95-115 has one invalid ID, 99.
-    998-1012 has one invalid ID, 1010.
-    1188511880-1188511890 has one invalid ID, 1188511885.
-    222220-222224 has one invalid ID, 222222.
-    1698522-1698528 contains no invalid IDs.
-    446443-446449 has one invalid ID, 446446.
-    38593856-38593862 has one invalid ID, 38593859.
-    The rest of the ranges contain no invalid IDs.
-
-	Add together all the invalid IDs for the output
 */
 
 long long pow10(long long n)
--- 2025/2b.cpp
+++ 2025/2b.cpp
@@ -11,27 +11,6 @@ using namespace std;
 	Day 2: "Gift Shop"
 	Input file contains product ID ranges left to check
 	Invalid IDs are made of a number repeated AT LEAST twice, or that start with 0
-
-	e.g for ranges:
-	11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
-	1698522-1698528,446443-446449,38593856-38593862,565653-565659,
-	824824821-824824827,2121212118-2121212124
-
-	Now we can repeat digits at least twice, so we have:
-    
-    11-22 still has two invalid IDs, 11 and 22.
-    95-115 now has two invalid IDs, 99 and 111.
-    998-1012 now has two invalid IDs, 999 and 1010.
-    1188511880-1188511890 still has one invalid ID, 1188511885.
-    222220-222224 still has one invalid ID, 222222.
-    1698522-1698528 still contains no invalid IDs.
-    446443-446449 still has one invalid ID, 446446.
-    38593856-38593862 still has one invalid ID, 38593859.
-    565653-565659 now has one invalid ID, 565656.
-    824824821-824824827 now has one invalid ID, 824824824.
-    2121212118-2121212124 now has one invalid ID, 2121212121.
-
-	Add together all the invalid IDs for the output
 */
 
 static int debug = 2;
