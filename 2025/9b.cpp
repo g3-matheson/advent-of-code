12d5da828199292733bc648d7dfcc48f7680f943
g3-matheson <g3.matheson@gmail.com>
2025-12-12 04:04:12 -0500

    day 9 done

--- 2025/9b.cpp
+++ 2025/9b.cpp
@@ -1,5 +1,6 @@
-#include <format>
+#include <climits>
 #include <iostream>
+#include <unordered_map>
 #include <unordered_set>
 #include <vector>
 
@@ -8,64 +9,32 @@
 using namespace std;
 using ll = long long;
 
-struct PairHash 
-{
-	size_t operator()(const pair<ll,ll>& p) const noexcept
-	{
-		return hash<ll>()(p.first) * 31 ^ hash<ll>()(p.second);
-	}
-};
-
-void fillGreens(unordered_set<pair<ll,ll>, PairHash>& greens)
-{
-	unordered_set<pair<ll,ll>, PairHash> newGreens;
-	for(auto& g1 : greens)
-	{
-		for(auto& g2 : greens)
-		{
-			if(g1.first == g2.first)
-			{
-				for(int y = min(g1.second, g2.second); y <= max(g1.second, g2.second); y++)
-				{
-					newGreens.insert({g1.first, y});
-				}
-			}
-			if(g1.second == g2.second)
-			{
-				for(int x = min(g1.first, g2.first); x <= max(g1.first, g2.first); x++)
-				{
-					newGreens.insert({x, g1.second});
-				}
-			}
-		}
-	}
-
-	for(auto& g : newGreens) greens.insert(g);
-}
-
-bool greenCheck(const vector<ll>&a, const vector<ll>&b, unordered_set<pair<ll,ll>, PairHash>& greens)
-{
-	for(ll x = min(a[0], b[0]); x <= max(a[0], b[0]); x++) 
-	{
-		if(!greens.contains({x, a[1]}) || !greens.contains({x, b[1]})) return false;
-	}
-	for(ll y = min(a[1], b[1]); y <= max(a[1], b[1]); y++)
-	{
-		if(!greens.contains({a[0], y}) || !greens.contains({b[0], y})) return false;
-	}
-	return true; 
-}
+/*
+	1) store green lines
+	2) for each fixed x, find intervals [y_i, y_j] that are in green area (same for y)
+	3) for each pair of red corners, check if all 4 border sides are in green area using (2)
+	4) compute area if (3) check passes
+*/
 
 ll area (const vector<ll>& x, const vector<ll>& y)
 {
-	return abs(x[0] - y[0] + 1) * abs(x[1] - y[1] + 1);
+	return (abs(x[0] - y[0]) + 1LL) * (abs(x[1] - y[1]) + 1LL);
+}
+
+// this could maybe be optimized but it runs fast enough
+bool intervalCheck(const vector<pair<ll,ll>>& intervals, ll low, ll high)
+{
+	for(auto& [l, r] : intervals)
+	{
+		if(l <= low && high <= r) return true;
+	}
+	return false;
 }
 
 int main()
 {
 	string s;
 	vector<vector<ll>> points;
-	unordered_set<pair<ll,ll>, PairHash>* greens = new unordered_set<pair<ll,ll>, PairHash>();
 
 	while(cin >> s)
 	{
@@ -75,34 +44,73 @@ int main()
 			| ranges::to<vector>());		
 	}
 
-	for(int i = 0; i < points.size(); i++)
+	int n = points.size();
+	unordered_map<ll, vector<pair<ll,ll>>> greensV, greensH;
+	unordered_set<ll> xs, ys;
+	for(int i = 0; i < n; i++)
+	{			
+		auto a = points[i], b = points[(i+1)%n];
+		xs.insert(a[0]);
+		xs.insert(b[0]);
+		ys.insert(a[1]);
+		ys.insert(b[1]);
+
+		if(a[1] == b[1]) greensH[a[1]].push_back({min(a[0], b[0]), max(a[0], b[0])});
+		if(a[0] == b[0]) greensV[a[0]].push_back({min(a[1], b[1]), max(a[1], b[1])});
+	}
+	
+	// find intervals along each fixed x,y value for which they are included in the green area
+	// < r/t exclusive to avoid double counting which would create intervals like (y1, y2), (y2, y3)
+	unordered_map<ll, vector<pair<ll,ll>>> xIntervals, yIntervals;
+	for(auto& x : xs)
 	{
-		vector<ll> a = points[i], b = points[(i+1) % points.size()];
-		for(ll x = min(a[0], b[0]); x <= max(a[0], b[0]); x++) 
+		vector<int> mergedY;
+		for(auto& [y, intervals] : greensH)
 		{
-			greens->insert({x, a[1]});
-			greens->insert({x, b[1]});
+			for(auto& [l, r] : intervals)
+			{
+				if(l <= x && x < r) mergedY.push_back(y);
+			}
 		}
-		for(ll y = min(a[1], b[1]); y <= max(a[1], b[1]); y++)
-		{
-			greens->insert({a[0], y});
-			greens->insert({b[0], y});
-		}
+		sort(mergedY.begin(), mergedY.end());
+		for(size_t i = 0; i+1 < mergedY.size(); i+=2) xIntervals[x].push_back({mergedY[i], mergedY[i+1]});
 	}
 
-	fillGreens(*greens);
+	for(auto& y : ys)
+	{
+		vector<int> mergedX;
+		for(auto& [x, intervals] : greensV)
+		{
+			for(auto& [b, t] : intervals)
+			{
+				if(b <= y && y < t) mergedX.push_back(x);
+			}
+		}
+		sort(mergedX.begin(), mergedX.end());
+		for(size_t i = 0; i+1 < mergedX.size(); i+=2) yIntervals[y].push_back({mergedX[i], mergedX[i+1]});
+	}
 
 	ll maxArea = 0;
-	for(int i = 0; i < points.size()-1; i++)
+
+	for(int i = 0; i < n-1; i++)
 	{
-		for(int j = i+1; j < points.size(); j++)
+		for(int j = i+1; j < n; j++)
 		{
-			if(greenCheck(points[i], points[j], *greens)) maxArea = max(maxArea, area(points[i], points[j]));
+			auto a = points[i], b = points[j];
+			if(a[0] == b[0] || a[1] == b[1]) continue; // 0 area 
+
+			auto minX = min(a[0], b[0]), maxX = max(a[0], b[0]);
+			auto minY = min(a[1], b[1]), maxY = max(a[1], b[1]);
+
+			if(intervalCheck(xIntervals[minX], minY, maxY) && intervalCheck(xIntervals[maxX], minY, maxY)
+			&& intervalCheck(yIntervals[minY], minX, maxX) && intervalCheck(yIntervals[maxY], minX, maxX))
+			{
+				maxArea = max(maxArea, area(a, b));
+			}
 		}
 	}
 
 	cout << maxArea << endl;
 
-	delete greens;
 	return 0;
 }
